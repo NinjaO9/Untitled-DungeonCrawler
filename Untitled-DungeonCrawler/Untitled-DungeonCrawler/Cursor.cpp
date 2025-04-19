25e873efdc1a3cd7f856d148c21f97be608d18db
MrThomasLibrary <beethoven@MyLaptop.somethingstupid.org>
2025-04-19 12:51:23 -0700

    cursor exists now

--- Untitled-DungeonCrawler/Untitled-DungeonCrawler/Cursor.cpp
+++ Untitled-DungeonCrawler/Untitled-DungeonCrawler/Cursor.cpp
@@ -9,5 +9,8 @@ Cursor::~Cursor() {
 }
 
 void Cursor::update() {
-   
+  const sf::Texture texture("Temp.png");
+  const sf::Sprite cursor(texture);
+  sf::Vector2i mousePos = sf::Mouse::getPosition();
+  cursor.setPosition(mousePos);
 }
