d9f862fac60f667bfbd22febfea65a33471902d0
David <davidmontesvilanova@gmail.com>
2025-04-23 15:33:21 -0700

    Offset exit tile by 1 to allow easy exiting

--- Untitled-DungeonCrawler/Untitled-DungeonCrawler/Obstacle.cpp
+++ Untitled-DungeonCrawler/Untitled-DungeonCrawler/Obstacle.cpp
@@ -4,5 +4,6 @@ Obstacle::Obstacle(sf::Vector2f pos)
 {
 	sprite = new sf::Sprite(TextureManager::getInstance()->getTexture("Wall"));
 	sprite->setPosition(pos);
+	sprite->setOrigin({ sprite->getPosition().x / 32, sprite->getPosition().y / 32 });
 	setPos(pos);
 }
