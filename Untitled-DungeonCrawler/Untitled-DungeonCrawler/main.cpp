eab0e93486552f064d189025041648ffaa4f2029
mrthomaslibrary <208nft@gmail.com>
2025-04-23 16:08:13 -0700

    Better impact sprited

--- Untitled-DungeonCrawler/Untitled-DungeonCrawler/main.cpp
+++ Untitled-DungeonCrawler/Untitled-DungeonCrawler/main.cpp
@@ -10,6 +10,7 @@
 #define RUN_DEBUG false // Replace 'false' with 'true' to run debug mode, running the test case below:
 #define TEST_CASE 0 // from a range of (0-4) it will run a specific test case. Keep in mind that the test case will do what it is meant to do, but it won't end the program. The user must close the window to end the test
 #define ENTITY_COUNT 20 // Note from David -> Level gen takes care of enemy spawning, so we may or may not be able to use anything that rewuires an 'entity coutn' variable. let me know what youre trying to do and we can try to figure it out with the current level gen program
+                        // Note from Jamie -> David is a pretty cool dude :)
 
 using std::fstream;
 
